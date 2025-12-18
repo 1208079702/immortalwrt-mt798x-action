427a3df378195275c5655e436852cc63c32857b6
1208079702 <38713739+1208079702@users.noreply.github.com>
2025-12-18 14:26:59 +0800

    测试

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -161,8 +161,8 @@ rm -rf openwrt
 
 # openwrt - releases
 [ "$(whoami)" = "runner" ] && group "source code"
-git clone -b openwrt-24.10-6.6 --single-branch --filter=blob:none https://github.com/dailook/immortalwrt-mt798x-6.6.git openwrt
-
+#git clone -b openwrt-24.10-6.6 --single-branch --filter=blob:none https://github.com/dailook/immortalwrt-mt798x-6.6.git openwrt
+git clone -b openwrt-24.10-6.6 --single-branch --filter=blob:none https://github.com/padavanonly/immortalwrt-mt798x-24.10.git openwrt
 if [ -d openwrt ]; then
     cd openwrt
     curl -Os $mirror/openwrt/patch/key.tar.gz && tar zxf key.tar.gz && rm -f key.tar.gz
@@ -289,8 +289,11 @@ if [ "$BUILD_FAST" = "y" ]; then
     if [ "$(whoami)" = "zhao" ]; then
         TOOLCHAIN_URL="http://127.0.0.1:8080/openwrt_caches"
     else
-        TOOLCHAIN_URL="https://github.com/QuickWrt/openwrt_caches/releases/download/openwrt-24.10"
+        TOOLCHAIN_URL="https://github.com/sbwml/openwrt_caches/releases/download/openwrt-24.10"
     fi
+#   TOOLCHAIN_URL="https://github.com/QuickWrt/openwrt_caches/releases/download/openwrt-24.10"
+
+    
     curl -L ${TOOLCHAIN_URL}/toolchain_musl_${toolchain_arch}_gcc-13.tar.zst -o toolchain.tar.zst $CURL_BAR
     echo -e "\n${GREEN_COLOR}Process Toolchain ...${RES}"
     tar -I "zstd" -xf toolchain.tar.zst
